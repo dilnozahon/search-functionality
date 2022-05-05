051b28a690ba2d146a59125dcbb5a75e1edc5c3a
dilya_me <inoplanetyanin0216@gmail.com>
2022-05-05 18:04:47 +0500

    search

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Ishchilar(models.Model):
+    class Meta:
+        verbose_name = 'Workers'
+        verbose_name_plural = 'Ishchilar'
+
+    ismi = models.CharField(max_length=255)
+    familiyasi = models.CharField(max_length=255)
+    yoshi = models.IntegerField(default=18)
+    kasbi = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.ismi
---
+++ core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Ishchilar
+from django.db.models import Q
+
+def home(request):
+    if 'q' in request.GET:
+        qidirish_sozi = request.GET['q']
+        toliq = Q(Q(ismi__icontains=qidirish_sozi) | Q(familiyasi__icontains=qidirish_sozi) | Q(yoshi__icontains=qidirish_sozi) | Q(kasbi__icontains=qidirish_sozi))
+        ishchilar = Ishchilar.objects.filter(toliq)
+    else:
+        ishchilar = Ishchilar.objects.all()
+    
+    context = {
+        'ishchilar': ishchilar
+    }
+    return render(request, 'index.html', context)
